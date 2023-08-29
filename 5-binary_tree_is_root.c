abb564e396df7a61a75c751ac01152cb274b1bc8
amel83 <tesfaneshk222@gmail.com>
2023-08-29 22:35:36 +0300

    3

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,8 +1,8 @@
-#include "binary_trees_t"
+#include "binary_trees.h"
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node != NULL || node->parent == NULL)
-		return (1);
-	return (0);
+	if (node == NULL || node->parent != NULL)
+		return (0);
+	return (1);
 }
