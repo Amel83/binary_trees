9d1a85093fab30361857cfd84079d02c58fd4eca
amel83 <tesfaneshk222@gmail.com>
2023-08-30 10:12:52 -0700

    ap

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,12 +6,16 @@
  * Return: return node count.
  */
 
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	if (tree->left->parent == NULL && tree->right->parent == NULL)
-		return (1);
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	size_t nodes = 0;
+
+	if (tree)
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
 }
 
