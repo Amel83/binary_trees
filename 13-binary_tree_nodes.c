60e03fcac94ff4d04d9ee5e6a6c65d4382e6142b
amel83 <tesfaneshk222@gmail.com>
2023-08-30 10:15:01 -0700

    ap

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - Counts the nodes which have min 1.
+ * binary_tree_nodes - Counts the nodes which have min 1.
  * @tree: pointer to the root node tree.
  * Return: return node count.
  */
